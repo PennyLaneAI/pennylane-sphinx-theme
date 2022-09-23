5eb2666db5ec0406786f196e254569c368340e21
Josh Izaac <josh146@gmail.com>
2022-09-23 18:37:02 -0400

    V0.3 (#3)
    ### Before submitting
    
    Please complete the following checklist when submitting a PR:
    
    - [ ] All new classes, functions, and members must be clearly commented
    and documented.
    
    - [ ] Ensure that code is properly formatted by running `make format
    lint`.
    
    - [ ] Add a new entry to [`CHANGELOG.md`](.github/CHANGELOG.md),
    summarizing the
          change and including a link back to the PR.
    
    - [ ] If applicable, include a screenshot or GIF to illustrate visual
    changes.
    
    When all the above are checked, delete everything above the dashed
    line and fill in the pull request template.
    
    
    ------------------------------------------------------------------------------------------------------------
    
    **Context:**
    
    **Description of the Change:**
    
    **Benefits:**
    
    **Possible Drawbacks:**
    
    **Related GitHub Issues:**

--- pennylane_sphinx_theme/_version.py
+++ pennylane_sphinx_theme/_version.py
@@ -1,6 +1,6 @@
 """
-This module specifies the Xanadu Sphinx Theme version with https://semver.org/
+This module specifies the PennyLane Sphinx Theme version with https://semver.org/
 using the following format: <major>.<minor>.<patch>[-<pre-release>].
 """
 
-__version__ = "0.3.0-dev"
+__version__ = "0.3.0"
