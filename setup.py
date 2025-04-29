3ec715e5895d07e7f9899900176d68a92b7561b6
Andrew Gardhouse <andrewgardhouse@gmail.com>
2025-04-29 11:36:03 -0400

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("pennylane_sphinx_theme/_version.py") as f:
 
 requirements = [
     "sphinx",
-    "xanadu-sphinx-theme~=0.11.0",
+    "xanadu-sphinx-theme~=0.12.0",
     # The packages below are used to generate thumbnail images.
     "pillow",
     "sphinx-gallery",
